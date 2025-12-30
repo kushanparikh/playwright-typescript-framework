c4ffd87970ea2545e75bb3832a8e58e7d7320f78
KushanParikh <parikhkushan@gmail.com>
2025-12-30 01:32:03 -0500

    Added first test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Able to login', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await expect(page).toHaveTitle('Swag Labs');
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  // Enter user name and password
+  await page.fill('input[name="user-name"]', 'standard_user');
+  await page.fill('input[name="password"]', 'secret_sauce');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // Click login button
+  await page.click('input[id="login-button"]');
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Verify login
+  await expect(page).toHaveTitle('Swag Labs');
 });
