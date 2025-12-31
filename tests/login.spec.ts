32b58909afa3cb3bbf2869634ca5b4df0491490d
KushanParikh <parikhkushan@gmail.com>
2025-12-30 20:08:28 -0500

    Added site URL into baseURL config for playwright.config.ts file

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
+  await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('Swag Labs');
