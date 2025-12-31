9590538de5c1375c49d4ce8ddbe8d73fe073b52d
KushanParikh <parikhkushan@gmail.com>
2025-12-30 20:01:44 -0500

    Implementated Role based locators

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -9,11 +9,11 @@ test.beforeEach(async ({ page }) => {
 
 test('should login successfully with valid credentials', async ({ page }) => {
   // Enter user name and password
-  await page.fill('input[name="user-name"]', 'standard_user');
-  await page.fill('input[name="password"]', 'secret_sauce');
+  await page.getByPlaceholder('Username').fill('standard_user');
+  await page.getByPlaceholder('Password').fill('secret_sauce');
 
   // Click login button
-  await page.click('input[id="login-button"]');
+  await page.getByRole('button', { name: 'Login' }).click();
 
   // Verify URL has changed
   await expect(page).toHaveURL(/inventory/);
@@ -21,11 +21,11 @@ test('should login successfully with valid credentials', async ({ page }) => {
 
 test('should show error for locked out user', async ({ page }) => {
   // Enter user name and password
-  await page.fill('input[name="user-name"]', 'locked_out_user');
-  await page.fill('input[name="password"]', 'secret_sauce');
+  await page.getByPlaceholder('Username').fill('locked_out_user');
+  await page.getByPlaceholder('Password').fill('secret_sauce');
 
   // Click login button
-  await page.click('input[id="login-button"]');
+  await page.getByRole('button', { name: 'Login' }).click();
 
   // Verify error message is displayed
   await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Sorry, this user has been locked out.');
@@ -33,11 +33,11 @@ test('should show error for locked out user', async ({ page }) => {
 
 test('should show error with non-existent username', async ({ page }) => {
   //Enter user name and password
-  await page.fill('input[name="user-name"]', 'invalid_user');
-  await page.fill('input[name="password"]', 'secret_sauce');
+  await page.getByPlaceholder('Username').fill('invalid_user');
+  await page.getByPlaceholder('Password').fill('secret_sauce');
 
   //Click login button
-  await page.click('input[id="login-button"]');
+  await page.getByRole('button', { name: 'Login' }).click();
 
   //Verify error message is displayed
   await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Username and password do not match any user in this service');
@@ -45,11 +45,11 @@ test('should show error with non-existent username', async ({ page }) => {
 
 test('should show error with empty password', async ({ page }) => {
   //Enter user name and password as empty string
-  await page.fill('input[name="user-name"]', 'standard_user');
-  await page.fill('input[name="password"]', '');
+  await page.getByPlaceholder('Username').fill('standard_user');
+  await page.getByPlaceholder('Password').fill('');
 
   //Click login button
-  await page.click('input[id="login-button"]');
+  await page.getByRole('button', { name: 'Login' }).click();
 
   //Verify error message is displayed
   await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Password is required');
@@ -57,10 +57,10 @@ test('should show error with empty password', async ({ page }) => {
 
 test('should show error with empty username', async ({ page }) => {
   //Enter password and keep username empty
-  await page.fill('input[name="password"]', 'secret_sauce');
+  await page.getByPlaceholder('Password').fill('secret_sauce');
 
   //Click login button
-  await page.click('input[id="login-button"]');
+  await page.getByRole('button', { name: 'Login' }).click();
 
   //Verify error message is displayed
   await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Username is required');
