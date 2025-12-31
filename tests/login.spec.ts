38972f5d68454e7754a20b9aa79438295ad6ce52
KushanParikh <parikhkushan@gmail.com>
2025-12-30 19:29:50 -0500

    Added a test for login error scenario

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -16,3 +16,20 @@ test('should login successfully with valid credentials', async ({ page }) => {
   // Verify URL has changed
   await expect(page).toHaveURL(/inventory/);
 });
+
+test('should get login error when using invalid credentials', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle('Swag Labs');
+
+  // Enter user name and password
+  await page.fill('input[name="user-name"]', 'locked_out_user');
+  await page.fill('input[name="password"]', 'secret_sauce');
+
+  // Click login button
+  await page.click('input[id="login-button"]');
+
+  // Verify error message is displayed
+  await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Sorry, this user has been locked out.');
+});
