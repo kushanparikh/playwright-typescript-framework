f973414951ac1a94d565a64cfc00e874fb4c2d12
KushanParikh <parikhkushan@gmail.com>
2025-12-30 19:36:33 -0500

    Added another scenario for non-existance username

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -17,7 +17,7 @@ test('should login successfully with valid credentials', async ({ page }) => {
   await expect(page).toHaveURL(/inventory/);
 });
 
-test('should get login error when using invalid credentials', async ({ page }) => {
+test('should show error for locked out user', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
 
   // Expect a title "to contain" a substring.
@@ -32,4 +32,21 @@ test('should get login error when using invalid credentials', async ({ page }) =
 
   // Verify error message is displayed
   await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Sorry, this user has been locked out.');
+});
+
+test('should show error with non-existent username', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  //Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle('Swag Labs');
+
+  //Enter user name and password
+  await page.fill('input[name="user-name"]', 'invalid_user');
+  await page.fill('input[name="password"]', 'secret_sauce');
+
+  //Click login button
+  await page.click('input[id="login-button"]');
+
+  //Verify error message is displayed
+  await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Username and password do not match any user in this service');
 });
