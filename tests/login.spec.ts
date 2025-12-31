86222f165afae92f1bde28935c91da8e0e3b9ce0
KushanParikh <parikhkushan@gmail.com>
2025-12-30 20:22:52 -0500

    Add a logic for adding screenshot

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -7,7 +7,7 @@ test.beforeEach(async ({ page }) => {
   await expect(page).toHaveTitle('Swag Labs');
 });
 
-test('should login successfully with valid credentials', async ({ page }) => {
+test('should login successfully with valid credentials', async ({ page }, testInfo) => {
   // Enter user name and password
   await page.getByPlaceholder('Username').fill('standard_user');
   await page.getByPlaceholder('Password').fill('secret_sauce');
@@ -17,9 +17,15 @@ test('should login successfully with valid credentials', async ({ page }) => {
 
   // Verify URL has changed
   await expect(page).toHaveURL(/inventory/);
+
+  // Attach screenshot to report
+  await testInfo.attach('inventory-page-after-login', {
+    body: await page.screenshot(),
+    contentType: 'image/png',
+  });
 });
 
-test('should show error for locked out user', async ({ page }) => {
+test('should show error for locked out user', async ({ page }, testInfo) => {
   // Enter user name and password
   await page.getByPlaceholder('Username').fill('locked_out_user');
   await page.getByPlaceholder('Password').fill('secret_sauce');
@@ -29,9 +35,15 @@ test('should show error for locked out user', async ({ page }) => {
 
   // Verify error message is displayed
   await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Sorry, this user has been locked out.');
+
+  // Attach screenshot to report
+  await testInfo.attach('login-page-after-login-error', {
+    body: await page.screenshot(),
+    contentType: 'image/png',
+  });
 });
 
-test('should show error with non-existent username', async ({ page }) => {
+test('should show error with non-existent username', async ({ page }, testInfo) => {
   //Enter user name and password
   await page.getByPlaceholder('Username').fill('invalid_user');
   await page.getByPlaceholder('Password').fill('secret_sauce');
@@ -41,9 +53,15 @@ test('should show error with non-existent username', async ({ page }) => {
 
   //Verify error message is displayed
   await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Username and password do not match any user in this service');
+
+  // Attach screenshot to report
+  await testInfo.attach('login-page-after-login-error', {
+    body: await page.screenshot(),
+    contentType: 'image/png',
+  });
 });
 
-test('should show error with empty password', async ({ page }) => {
+test('should show error with empty password', async ({ page }, testInfo) => {
   //Enter user name and password as empty string
   await page.getByPlaceholder('Username').fill('standard_user');
   await page.getByPlaceholder('Password').fill('');
@@ -53,9 +71,15 @@ test('should show error with empty password', async ({ page }) => {
 
   //Verify error message is displayed
   await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Password is required');
+
+  // Attach screenshot to report
+  await testInfo.attach('login-page-after-login-error', {
+    body: await page.screenshot(),
+    contentType: 'image/png',
+  });
 });
 
-test('should show error with empty username', async ({ page }) => {
+test('should show error with empty username', async ({ page }, testInfo) => {
   //Enter password and keep username empty
   await page.getByPlaceholder('Password').fill('secret_sauce');
 
@@ -64,4 +88,10 @@ test('should show error with empty username', async ({ page }) => {
 
   //Verify error message is displayed
   await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Username is required');
+
+  // Attach screenshot to report
+  await testInfo.attach('login-page-after-login-error', {
+    body: await page.screenshot(),
+    contentType: 'image/png',
+  });
 });
